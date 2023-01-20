98186506693b56687da17063dc325b36c1e8d90c
Mockingbird Emmaline <mockingb@ev-j2.kzn.21-school.ru>
2023-01-20 15:11:35 +0300

    d

--- viewer.cpp
+++ viewer.cpp
@@ -1,80 +1,97 @@
 #include "viewer.h"
-#include "ui_viewer.h"
-#include <QFileDialog>
+
 #include <QDebug>
+#include <QFileDialog>
 #include <iostream>
 
-viewer::viewer(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::viewer)
-{
-    ui->setupUi(this);
-    obj.count_of_vertexes = 0;
-    obj.count_of_facets = 0;
-    }
+#include "ui_viewer.h"
 
-viewer::~viewer()
-{
-    delete ui;
+viewer::viewer(QWidget* parent) : QMainWindow(parent), ui(new Ui::viewer) {
+  setlocale(LC_ALL, "en_US.UTF-8");
+  ui->setupUi(this);
+  obj.count_of_vertexes = 0;
+  obj.count_of_facets = 0;
 }
 
+viewer::~viewer() { delete ui; }
 
-void viewer::on_pushButton_clicked()
-{
-//    static double vertex_ar[] = {-0.500000, -0.500000, 0.500000,
-//                          0.500000, -0.500000, 0.500000,
-//                          -0.500000, 0.500000, 0.500000,
-//                          0.500000, 0.500000, 0.500000,
-//                          -0.500000, 0.500000, -0.500000,
-//                          0.500000, 0.500000, -0.500000,
-//                          -0.500000, -0.500000, -0.500000,
-//                          0.500000, -0.500000, -0.500000};
-//    static int indexes_ar[] = {0,1,1,2,2,0, 2,1,1,3,3,2, 2,3,3,4,4,2, 4,3,3,5,5,4,
-//                        4,5,5,6,6,4, 6,5,5,7,7,6, 6,7,7,0,0,6, 0,7,7,1,1,0,
-//                        1,7,7,3,3,1, 3,7,7,5,5,3, 6,0,0,4,4,6, 4,0,0,2,2,4};
-//    ui->widget->set_vertex_arr((vertex_ar));
-//    ui->widget->set_facets_arr(indexes_ar);
-//    ui->widget->set_lines(72);
-//    ui->widget->set();
-//    ui->widget->update();
+void viewer::on_pushButton_clicked() {
+  //    static double vertex_ar[] = {-0.500000, -0.500000, 0.500000,
+  //                          0.500000, -0.500000, 0.500000,
+  //                          -0.500000, 0.500000, 0.500000,
+  //                          0.500000, 0.500000, 0.500000,
+  //                          -0.500000, 0.500000, -0.500000,
+  //                          0.500000, 0.500000, -0.500000,
+  //                          -0.500000, -0.500000, -0.500000,
+  //                          0.500000, -0.500000, -0.500000};
+  //    static int indexes_ar[] = {0,1,1,2,2,0, 2,1,1,3,3,2, 2,3,3,4,4,2,
+  //    4,3,3,5,5,4,
+  //                        4,5,5,6,6,4, 6,5,5,7,7,6, 6,7,7,0,0,6, 0,7,7,1,1,0,
+  //                        1,7,7,3,3,1, 3,7,7,5,5,3, 6,0,0,4,4,6, 4,0,0,2,2,4};
+  //    ui->widget->set_vertex_arr((vertex_ar));
+  //    ui->widget->set_facets_arr(indexes_ar);
+  //    ui->widget->set_lines(72);
+  //    ui->widget->set();
+  //    ui->widget->update();
 
-    QString fileName = QFileDialog::getOpenFileName(this,
-        tr("Open Obj File"), "/Users/", tr("OBJ Files (*.obj)"));
-    if(fileName!="") {
-        qDebug() << fileName;
+  QString fileName = QFileDialog::getOpenFileName(
+      this, tr("Open Obj File"), "/Users/", tr("OBJ Files (*.obj)"));
+  if (fileName != "") {
+    qDebug() << fileName;
 
-        QByteArray tmp = fileName.toLocal8Bit();
-        char* file = tmp.data();
+    QByteArray tmp = fileName.toLocal8Bit();
+    char* file = tmp.data();
 
-        int err = readobj(file, &obj);
+    int err = readobj(file, &obj);
 
-        if(err!=-1){
-            ////scaling block
-            ///
-            double max_el = 0.0;
-            for (int i = 0; i < obj.count_of_vertexes; i++) {
-                  if (max_el < obj.vertexes[i]) max_el = obj.vertexes[i];
-                }
+    if (err != -1) {
+      ////scaling block
+      ///
+      double max_el = 0.0;
+      for (int i = 0; i < obj.count_of_vertexes; i++) {
+        if (max_el < obj.vertexes[i]) max_el = obj.vertexes[i];
+      }
 
-            for (int i = 0; i < (obj.count_of_vertexes)*3; i++) {
-                  obj.vertexes[i] /= max_el;
-                }
-            ////end scaling block
-            ///
-            ui->widget->set_vertex_arr(obj.vertexes);
-            ui->widget->set_facets_arr(obj.polygons);
-            for(int i = 0; i < err;i++) {
-                std::cout<<obj.polygons[i]<<std::endl;
-            }
-            ui->widget->set_lines(err);
-            ui->widget->set();
-            ui->widget->update();
-        }
-
+      for (int i = 0; i < (obj.count_of_vertexes) * 3; i++) {
+        obj.vertexes[i] /= max_el;
+      }
+      ////end scaling block
+      ///
+      ui->widget->set_vertex_arr(obj.vertexes);
+      ui->widget->set_facets_arr(obj.polygons);
+      for (int i = 0; i < err; i++) {
+        std::cout << obj.polygons[i] << std::endl;
+      }
+      ui->widget->set_lines(err);
+      ui->widget->set();
+      ui->widget->update();
     }
+  }
+}
 
+void viewer::on_pushButton_2_clicked() { QApplication::quit(); }
 
+void viewer::on_pushButton_4_clicked() {
+  for (int i = 0; i < (obj.count_of_vertexes) * 3; i++) {
+    obj.vertexes[i] /= 0.95;
+  }
+  ui->widget->set_vertex_arr(obj.vertexes);
+  ui->widget->update();
+}
 
+void viewer::on_pushButton_3_clicked() {
+  for (int i = 0; i < (obj.count_of_vertexes) * 3; i++) {
+    obj.vertexes[i] *= 0.95;
+  }
+  ui->widget->set_vertex_arr(obj.vertexes);
+  ui->widget->update();
+}
 
-
+void viewer::on_pushButton_5_clicked() {
+  for (int i = 0, k = 1; i < (obj.count_of_vertexes) * 3; k++, i++) {
+    if (k == 1) obj.vertexes[i] += 0.1;
+    if (k == 3) k = 1;
+  }
+  ui->widget->set_vertex_arr(obj.vertexes);
+  ui->widget->update();
 }
--- viewer.h
+++ viewer.h
@@ -24,8 +24,17 @@ public:
 private slots:
     void on_pushButton_clicked();
 
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_4_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_5_clicked();
+
 private:
     Ui::viewer *ui;
+    int dots = 0;
 
 
 };
